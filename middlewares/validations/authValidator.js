8cc3a2131c8a180104f3e4d5be845098163680ec
KBelltree <98202109+kbelltree@users.noreply.github.com>
2026-02-20 17:32:43 -0800

    Update authValidator

--- middlewares/validations/authValidator.js
+++ middlewares/validations/authValidator.js
@@ -1,27 +1,67 @@
 const { body, validationResult } = require('express-validator');
 
 const validateNewUser = [
-  body('email').trim().notEmpty().isEmail().toLowerCase(),
-  body('username').trim().isLength({ min: 1, max: 15 }),
-  body('password').trim().notEmpty(),
+  body('email')
+    .trim()
+    .notEmpty()
+    .withMessage('Email is required.')
+    .isEmail()
+    .withMessage('Valid email is required.')
+    .normalizeEmail(),
+
+  body('username')
+    .trim()
+    .notEmpty()
+    .withMessage('Username is required.')
+    .isLength({ min: 1, max: 15 })
+    .withMessage('Username must be between 1 to 15 characters.'),
+
+  body('password')
+    .trim()
+    .notEmpty()
+    .withMessage('Password is required.')
+    .isLength({ min: 8 })
+    .withMessage('Password must have at least 8 characters.'),
+
   (req, res, next) => {
     const errors = validationResult(req);
+
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+      // All errors is returned in string instead of array list
+      const errorMessage = errors
+        .array()
+        .map((error) => error.msg)
+        .join();
+      return res.status(400).json({ error: errorMessage });
     }
+
     return next();
   },
 ];
 
 const validateLogInItems = [
-  body('email').trim().notEmpty().isEmail().toLowerCase(),
-  body('password').trim().notEmpty(),
+  body('email')
+    .trim()
+    .notEmpty()
+    .withMessage('Email is required.')
+    .isEmail()
+    .withMessage('Valid email is required.')
+    .normalizeEmail(),
+
+  body('password').trim().notEmpty().withMessage('Password is required.'),
+
   (req, res, next) => {
     const errors = validationResult(req);
+
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+      const errorMessage = errors
+        .array()
+        .map((error) => error.msg)
+        .join();
+      return res.status(400).json({ error: errorMessage });
     }
-    return next();
+
+    next();
   },
 ];
 
